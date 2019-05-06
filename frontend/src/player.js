4928616f36d29f26c7484cb625a489b81b583848
github username <github email address>
2019-05-06 13:19:49 -0400

    Got player working with border collision

--- frontend/src/player.js
+++ frontend/src/player.js
@@ -1,1 +1,47 @@
 // for wip purposes
+/******************************************************************************
+* player canvas element
+******************************************************************************/
+let playerRadius = 50;
+let pX = canvas.width/2;
+let pY = canvas.height/2;
+let pDx = 10;
+let pDy = 10;
+
+function drawPlayer(){
+  ctx.beginPath();
+  ctx.arc(pX, pY, playerRadius, 0, Math.PI*2);
+  ctx.fillStyle = "red";
+  ctx.fill();
+  ctx.closePath();
+};
+
+function doKeyDown(e){
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  switch (e.keyCode) {
+    case 38:  /* Up arrow was pressed */
+      if (pY > playerRadius){
+      pY -= pDy;
+      }
+      break;
+    case 40:  /* Down arrow was pressed */
+      if (pY < canvas.height - playerRadius){
+      pY += pDy;
+      }
+      break;
+    case 37:  /* Left arrow was pressed */
+      if (pX > playerRadius){
+      pX -= pDx;
+      }
+      break;
+    case 39:  /* Right arrow was pressed */
+      if (pX < canvas.width - playerRadius){
+      pX += pDx;
+      }
+      break;
+  }
+}
+
+document.addEventListener("keydown", doKeyDown, true)
+
+setInterval(drawPlayer, 5)
