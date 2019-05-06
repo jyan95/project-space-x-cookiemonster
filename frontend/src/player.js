99408f48c10d356ae47e3f9d0f6c0ad7b33de5e2
github username <github email address>
2019-05-06 15:41:07 -0400

    Got diagonal movement working. Although not smooth

--- frontend/src/player.js
+++ frontend/src/player.js
@@ -16,32 +16,90 @@ function drawPlayer(){
   ctx.closePath();
 };
 
+//direction keys
+let direction = {};
 function doKeyDown(e){
   ctx.clearRect(0, 0, canvas.width, canvas.height)
-  switch (e.keyCode) {
-    case 38:  /* Up arrow was pressed */
-      if (pY > pR){
-      pY -= pDy;
+  onkeydown = onkeyup = function(e){
+    direction[e.keyCode] = e.type == 'keydown';
+    console.log(direction)
+    /* insert conditional here */
+    //Up and left
+    if (direction[38] && direction[37]) {
+      if (pY > pR && pX > pR) {
+        pY -= pDy;
+        pX -= pDx;
       }
-      break;
-    case 40:  /* Down arrow was pressed */
-      if (pY < canvas.height - pR){
-      pY += pDy;
+    }
+    //Down and left
+    else if (direction[40] && direction[37]) {
+      if (pY < canvas.height - pR && pX > pR) {
+        pY += pDy;
+        pX -= pDx;
       }
-      break;
-    case 37:  /* Left arrow was pressed */
-      if (pX > pR){
-      pX -= pDx;
+    }
+    //Up and right
+    else if (direction[38] && direction[39]) {
+      if (pY > pR && pX < canvas.width - pR) {
+        pY -= pDy;
+        pX += pDx;
       }
-      break;
-    case 39:  /* Right arrow was pressed */
-      if (pX < canvas.width - pR){
-      pX += pDx;
+    }
+    //Down and right
+    else if (direction[40] && direction[39]) {
+      if (pY < canvas.height - pR && pX < canvas.width - pR) {
+        pY += pDy;
+        pX += pDx;
       }
-      break;
+    }
+    //Up
+    else if (direction[38]) {
+      if (pY > pR) {
+        pY -= pDy;
+      }
+    }
+    //Down
+    else if (direction[40]) {
+      if (pY < canvas.height - pR) {
+        pY += pDy;
+      }
+    }
+    //Left
+    else if (direction[37]) {
+      if (pX > pR) {
+        pX -= pDx;
+      }
+    }
+    //Right
+    else if (direction[39]) {
+      if (pX < canvas.width - pR) {
+        pX += pDx;
+      }
+    }
+    // switch (direction) {
+    //   case direction["38"]:  /* Up arrow was pressed */
+    //   if (pY > pR){
+    //     pY -= pDy;
+    //   }
+    //   break;
+    //   case 40:  /* Down arrow was pressed */
+    //   if (pY < canvas.height - pR){
+    //     pY += pDy;
+    //   }
+    //   break;
+    //   case 37:  /* Left arrow was pressed */
+    //   if (pX > pR){
+    //     pX -= pDx;
+    //   }
+    //   break;
+    //   case 39:  /* Right arrow was pressed */
+    //   if (pX < canvas.width - pR){
+    //     pX += pDx;
+    //   }
+    //   break;
+    // }
   }
 }
 
 document.addEventListener("keydown", doKeyDown, true)
-
 // setInterval(drawPlayer, 5)
